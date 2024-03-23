c024ac246fc7d0dd4bad29fe696d3321307219e3
Brian Gerke <brian@recurve.com>
2024-03-23 17:32:43 +0000

    hook in sigma data and change view

--- src/App.js
+++ src/App.js
@@ -1,33 +1,61 @@
 import React, { useState, useRef } from "react";
-import useSwr from "swr";
+//import useSwr from "swr";
 import ReactMapGL, { Marker, FlyToInterpolator } from "react-map-gl";
 import useSupercluster from "use-supercluster";
+import {
+    client,
+    useConfig,
+    useElementColumns,
+    useElementData,
+  } from "@sigmacomputing/plugin";
 import "./App.css";
 
-const fetcher = (...args) => fetch(...args).then(response => response.json());
+//const fetcher = (...args) => fetch(...args).then(response => response.json());
+
+client.config.configureEditorPanel([
+    { name: "source", type: "element" },
+    { name: "Latitude", type: "column", source: "source", allowMultiple: false, allowedTypes: ['number', 'integer'] },
+    { name: "Longitude", type: "column", source: "source", allowMultiple: false, allowedTypes: ['number', 'integer'] },
+    { name: "Subpopulation", type: "column", source: "source", allowMultiple: false, allowedTypes: ['integer','boolean'] },
+    { name: "Cluster scale", type: "text", placeholder: 'Number from 1 to 100' },
+  ]);
 
 export default function App() {
+
+  const config = useConfig();
+  const columns = useElementColumns(config.source);
+  const sigmaData = useElementData(config.source);
+  
+  const data = React.useMemo(() => {
+    const latitude = config.Latitude;
+    const longitude = config.Longitude;
+    const subpop = config.Subpopulation;
+    // console.log('got data', config, sigmaData, columns, latitude, longitude, fields);
+  }, 
+  [columns, config.Latitude, config.Longitude, config.Subpopulation, sigmaData]);
+
+  //TODO: make the sizing below dynamic
   const [viewport, setViewport] = useState({
-    latitude: 52.6376,
-    longitude: -1.135171,
+    latitude: 34.0549,
+    longitude: -118.2426,
     width: "100vw",
     height: "100vh",
-    zoom: 12
+    zoom: 8
   });
   const mapRef = useRef();
 
-  const url =
-    "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2023-10";
-  const { data, error } = useSwr(url, { fetcher });
-  const crimes = data && !error ? data.slice(0, 2000) : [];
-  const points = crimes.map(crime => ({
+  //const url =
+  //  "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2023-10";
+  //const { data, error } = useSwr(url, { fetcher });
+  const sites = data ? data.slice(0, 2000) : [];
+  const points = sites.map(site => ({
     type: "Feature",
-    properties: { cluster: false, crimeId: crime.id, category: crime.category },
+    properties: { cluster: false},
     geometry: {
       type: "Point",
       coordinates: [
-        parseFloat(crime.location.longitude),
-        parseFloat(crime.location.latitude)
+        site.longitude,
+        site.latitude
       ]
     }
   }));
