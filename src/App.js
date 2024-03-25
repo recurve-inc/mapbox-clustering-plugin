2f5c044053c81e52cfb047a5c1b5b8399c23585a
Brian Gerke <brian@recurve.com>
2024-03-25 05:43:02 +0000

    clusters plotting, not points yet.

--- src/App.js
+++ src/App.js
@@ -23,16 +23,36 @@ client.config.configureEditorPanel([
 export default function App() {
 
   const config = useConfig();
-  const columns = useElementColumns(config.source);
+  //const columns = useElementColumns(config.source);
   const sigmaData = useElementData(config.source);
   
   const data = React.useMemo(() => {
     const latitude = config.Latitude;
     const longitude = config.Longitude;
-    const subpop = config.Subpopulation;
+    const subpopulation = config.Subpopulation;
     // console.log('got data', config, sigmaData, columns, latitude, longitude, fields);
+    const _data = [];
+    if (sigmaData?.[latitude] && sigmaData?.[longitude]) {
+        for (let i = 0; i < sigmaData[latitude].length; i++) {
+          const lat = sigmaData[latitude][i];
+          const long = sigmaData[longitude][i];
+          const spop = sigmaData[subpopulation][i];
+          //const tooltips = fields.map(id => {
+          //  return {
+          //    key: columns[id]?.name || '',
+          //    value: sigmaData[id] ? sigmaData[id][i] : '',
+          //  };
+          //}); 
+          _data.push({
+            id: i,
+            coordinates: [long, lat],
+            subpop: spop
+          })
+        }
+    }
+    return _data
   }, 
-  [columns, config.Latitude, config.Longitude, config.Subpopulation, sigmaData]);
+  [config.Latitude, config.Longitude, config.Subpopulation, sigmaData]);
 
   //TODO: make the sizing below dynamic
   const [viewport, setViewport] = useState({
@@ -47,17 +67,16 @@ export default function App() {
   //const url =
   //  "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2023-10";
   //const { data, error } = useSwr(url, { fetcher });
-  const sites = data ? data.slice(0, 2000) : [];
+  const sites = data ? data : [];
+
   const points = sites.map(site => ({
     type: "Feature",
-    properties: { cluster: false, subpop: site.subpop},
+    properties: { cluster: false},
     geometry: {
       type: "Point",
-      coordinates: [
-        site.longitude,
-        site.latitude
-      ],
-    }
+      coordinates: site.coordinates,
+    },
+    subpop: site.subpop
   }));
 
   const bounds = mapRef.current
@@ -72,7 +91,7 @@ export default function App() {
     points,
     bounds,
     zoom: viewport.zoom,
-    options: { radius: 75, maxZoom: 17 }
+    options: { radius: 75, maxZoom: 15 }
   });
 
   return (
@@ -137,10 +156,12 @@ export default function App() {
               latitude={latitude}
               longitude={longitude}
               subpop={subpop}
-              color={this.subpop ? 'green' : 'gray'}
+              color='gray'//{subpop ? 'green' : 'gray'}
             >
              <div
                   className="site-marker"
+                  style={{width:`10px`, height:`10px`}}
+                  color='gray'
               ></div>
               {//<button className="crime-marker">
                 //<img src="/custody.svg" alt="crime doesn't pay" />
